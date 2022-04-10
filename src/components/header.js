f16011822368215b1d7bfcc312aee5e4e9047b91
Nidhi Tholar <nidhi.tholarkuchur@sjsu.edu>
2022-04-10 13:26:09 -0700

    minor additions

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ const Header = () => {
     return (
             <div className="row header">
                 <div className="col-md-4 header-elements">
-                    <span>Logo</span>
+                    <span>MRFS</span>
                 </div>
                 <div className="col-md-8 header-elements">
                     <span>Macroeconomic Researcher Food Security Time Series Dashboard</span>
--- src/components/imports.js
+++ src/components/imports.js
@@ -13,7 +13,30 @@ const Imports = () => {
                 <div className="col-md-1 vl"></div>
                 
                 <div className='col-md-8 main-container'>
+                    <div className="row ">
+                        <div className="col-md-9"></div>
+                            <div className="col-md-3">
+                                <select >
+                                    <option>USA</option>
+                                    <option>CHINA</option>
+                                    <option>INDIA</option>
+                                </select>
+                            </div>
+                        </div>
                     <div className="row">
+                        <div className="col-md-6">
+                            <select>
+                                <option>2015</option>
+                                <option>2016</option>
+                            </select>
+                        </div>
+                        <div className="col-md-6">
+                            <button>Wheat</button>
+                            <button>Rice</button>
+                            <button>Corn</button>
+                        </div>
+                    </div>
+                    <div className="row ">
                         <div className="col-md-7">sankey</div>
                         <div className="col-md-5">pie</div>
                     </div>
