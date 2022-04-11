8db9c9db8a75596f4fac0da9b0902275dfbb4a6e
Your Name <you@example.com>
2022-04-10 20:52:48 -0700

    datafile

--- src/components/imports.js
+++ src/components/imports.js
@@ -1,49 +1,186 @@
 import Header from "./header";
 import NavBar from "./nav";
+import {useState,useEffect} from 'react'
 import '../App.css';
-
+import {importData} from './data';
+import { Chart } from "react-google-charts";
+//import Highcharts from "highcharts";
+//import HighchartsSankey from "highcharts/modules/sankey";
+//import HighchartsReact from "highcharts-react-official";
 const Imports = () => {
-    return (
-        <div>
-             <div className="row">
-                <Header/>
-            </div>
-            <div className='row'>
-                <div className='col-md-3'><NavBar/></div>
-                <div className="col-md-1 vl"></div>
-                
-                <div className='col-md-8 main-container'>
-                    <div className="row ">
-                        <div className="col-md-9"></div>
+    const [allyears, setAllYears] = useState([])
+    const [data,setData] = useState(importData)
+    const [year,selectedYear] = useState()
+    const [country,selectedCountry] = useState('Egypt')
+    const [sankeyData,setSankeyData] = useState([])
+    const [pieData,setPieData] = useState([])
+    var Sanky_Options = {
+        title: 'Sanky Chart',
+        legend: {
+            position: "bottom",
+            alignment: "center",
+        },
+
+        chartArea:{
+            width: "90%",
+            height: "90",
+            left: '0%', top: '1%',forceIFrame:true
+        },
+    }
+    var pie_options = {
+        title: 'Pie Chart',
+        legend: {
+            position: "bottom",
+            alignment: "center",
+        },
+
+        chartArea:{
+            width: "90%",
+            height: "90%",
+        },
+
+    };
+    const filterData = (value)=>{
+         const yearFilteredData = importData.filter(item=>(item[3].toString() === year))
+        const countryFilteredData = yearFilteredData.filter(item=>(item[1].toString() === country))
+        const foodTypeFilteredData = countryFilteredData.filter(item=>(item[0].toString() === value))
+        let filteredDataforSankey = [["From","To","Qty"]]
+        for (let i=0;i<foodTypeFilteredData.length;i++){
+            let row = []
+            row.push(foodTypeFilteredData[i][2])
+            row.push(foodTypeFilteredData[i][1])
+            row.push(foodTypeFilteredData[i][4])
+            filteredDataforSankey.push(row)
+        }
+
+        let filteredDataforpie = [["From","Qty"]]
+        for (let i=0;i<foodTypeFilteredData.length;i++){
+            let row = []
+            row.push(foodTypeFilteredData[i][2])
+            row.push(foodTypeFilteredData[i][4])
+            filteredDataforpie.push(row)
+        }
+
+        setData(foodTypeFilteredData)
+        console.log(filteredDataforSankey)
+        console.log(filteredDataforpie)
+        setSankeyData(filteredDataforSankey)
+        setPieData(filteredDataforpie)
+
+    }
+
+    const getAllyearsfromArray = (arr) => {
+        const lastIndex = []
+        for (let i = 0; i < arr.length; i++) {
+            lastIndex.push(arr[i][arr[i].length - 2])
+
+        }
+        const uniqueyrs =  [...new Set(lastIndex)]
+        setAllYears(uniqueyrs)
+    }
+        useEffect(() => {
+            getAllyearsfromArray(importData)
+        }, [data]) //[] dependency array if we need options on useEffect
+        return (
+            <div>
+                <div className="row">
+                    <Header/>
+                </div>
+                <div className='row'>
+                    <div className='col-md-3'><NavBar/></div>
+                    <div className="col-md-1 vl"></div>
+
+                    <div className='col-md-8'>
+                        <div className="row ">
+                            <div className="col-md-9"></div>
                             <div className="col-md-3">
-                                <select >
-                                    <option>USA</option>
-                                    <option>CHINA</option>
-                                    <option>INDIA</option>
+                                <select onChange={e => selectedCountry(e.target.value)}>
+                                    <option value='Egypt'>Egypt</option>
+                                    <option value='Saudi Arabia'>Saudi</option>
                                 </select>
                             </div>
                         </div>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <select>
-                                <option>2015</option>
-                                <option>2016</option>
-                            </select>
+                        <div className="row">
+                            <div className="col-md-4">
+                                <select
+                                        onChange={e => selectedYear(e.target.value)}>
+                                    {allyears&& allyears.map(year=>
+                                        <option value = {year} >
+                                        {year}
+                                        </option>)}
+
+                                </select>
+                            </div>
+                            <div className="col-md-6">
+                                <button value ='Wheat' onClick={(e)=>filterData(e.target.value)}>Wheat</button>
+                                <button value ='Rice' onClick={(e)=>filterData(e.target.value)}>Rice</button>
+                            </div>
+
                         </div>
-                        <div className="col-md-6">
-                            <button>Wheat</button>
-                            <button>Rice</button>
-                            <button>Corn</button>
+                        <div className="row">
+                            <div className="col-md-5">
+                                {sankeyData.length>1&& <Chart
+                                    chartType="Sankey"
+                                    data={sankeyData}
+                                    width="1000px"
+                                    height = "500px"
+                                    options ={Sanky_Options}
+                                />
+
+
+
+                                }
+
+                            </div>
+
                         </div>
-                    </div>
-                    <div className="row ">
-                        <div className="col-md-7">sankey</div>
-                        <div className="col-md-5">pie</div>
+                       <div>
+                            <div className="col-md-5">
+                                {pieData.length>1&& <Chart
+                                    chartType="PieChart"
+                                    data={pieData}
+                                    width="1000px"
+                                    height = "500px"
+                                    options ={pie_options}
+                                />
+                                }
+
+                            </div>
+                        </div>
+                        {/*
+
+                                                <div className='row'>
+                            <table className='table table-striped'>
+                                <thead>
+                                <td>Importing item</td>
+                                <td>importing country</td>
+                                <td>Imported from</td>
+                                <td>import year</td>
+                                <td>import volume</td>
+                                </thead>
+                                <tbody>
+                                {data && data.map(item => (
+                                    <tr>
+                                        <td>{item[0]}</td>
+                                        <td>{item[1]}</td>
+                                        <td>{item[2]}</td>
+                                        <td>{item[3]}</td>
+                                        <td>{item[4]}</td>
+                                    </tr>
+                                ))}
+
+                                </tbody>
+
+
+                            </table>
+
+                        </div>
+                        */}
+
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
 }
+export default Imports
 
-export default Imports;
